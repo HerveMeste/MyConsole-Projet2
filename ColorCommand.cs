5e81429fef6d09194b5fe5ce6c9038b881900521
IzyJu <julienizd11@gmail.com>
2020-11-05 10:25:04 +0100

    Ajout command color, color fg, color bg

---
+++ ColorCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_Console_Text
+{
+    public class ColorCommand : BaseCommand
+    {
+        public override String Name { get => "color"; }
+        public override void Execute(String[] fullCommand)
+        {
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            if (fullCommand.Length == 1)
+            {
+                Console.WriteLine(String.Join("\n", colors));
+            }
+            else if (fullCommand.Length == 3 && fullCommand[1] == "fg")
+            {
+                foreach (var color in colors)
+                {
+                    if (Convert.ToString(color) == fullCommand[2])
+                    {
+                        Console.ForegroundColor = color;
+                        return;
+                    }
+                }
+                Console.WriteLine("La couleur ou la syntaxe n'existe pas, taper 'color' pour voir la liste des couleurs disponible");
+            }
+            else if (fullCommand.Length == 3 && fullCommand[1] == "bg")
+            {
+                foreach (var color in colors)
+                {
+                    if (Convert.ToString(color) == fullCommand[2])
+                    {
+                        Console.BackgroundColor = color;
+                        Console.Clear();
+                        return;
+                    }
+                }
+                Console.WriteLine("La couleur ou la syntaxe n'existe pas, taper 'color' pour voir la liste des couleurs disponible");
+            }
+        }
+    }
+}
--- MyConsole.cs
+++ MyConsole.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 
 namespace My_Console_Text
 {
     public class MyConsole
     {
-        private readonly BaseCommand[] _avalaibleCommands = { new ChangeDirectoryCommand(), new ListDirectoryCommand() };
+        private readonly BaseCommand[] _avalaibleCommands = { new ChangeDirectoryCommand(), new ListDirectoryCommand(), new ColorCommand() };
         List<string> history = new List<string>();
 
         public void Run()
