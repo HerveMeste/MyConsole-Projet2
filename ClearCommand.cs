ffd6c8377519f196679d3110d00f9b7b2b5e6ceb
herve.meste@gmail.com <herve.meste@gmail.com>
2020-11-05 12:38:50 +0100

    Add clear command,pwd command

---
+++ ClearCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace My_Console_Text
+{
+    public class ClearCommand : BaseCommand
+    {
+        public override String Name { get => "cls"; }
+
+        public override void Execute(String[] fullCommand)
+        {
+            Console.Clear();
+        }
+    }
+}
